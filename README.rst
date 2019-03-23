a6754123780d668cb4e01032a56b548c07cda81d
Uros Trstenjak <uros.trstenjak@gmail.com>
2019-03-24 00:29:06 +0100

    initial commit

---
+++ README.rst
@@ -0,0 +1,5 @@
+================
+React Voting App
+================
+
+App #1: Learning React - Fullstack React the Complete Guide to ReactJS and Friends
---
+++ src/Components.js
@@ -0,0 +1,109 @@
+import React from 'react';
+
+import 'semantic-ui-css/semantic.min.css';
+
+const products = [
+  {
+    "id": 1,
+    "title": "Yellow Lorem",
+    "description": "Lorem Ipsum is simply dummy text of the printing industry.",
+    "url": "#",
+    "votes": 41,
+    "submitterAvatarUrl": "images/avatars/avatar1.png",
+    "productImageUrl": "images/products/yellow.jpeg"
+  },
+  {
+    "id": 2,
+    "title": "Red Lorem",
+    "description": "Lorem Ipsum is simply dummy text of the printing industry.",
+    "url": "#",
+    "votes": 13,
+    "submitterAvatarUrl": "images/avatars/avatar2.png",
+    "productImageUrl": "images/products/red.png"
+  },
+  {
+    "id": 3,
+    "title": "Blue Lorem",
+    "description": "Lorem Ipsum is simply dummy text of the printing industry.",
+    "url": "#",
+    "votes": 26,
+    "submitterAvatarUrl": "images/avatars/avatar3.png",
+    "productImageUrl": "images/products/blue.png"
+  },
+  {
+    "id": 4,
+    "title": "Green Lorem",
+    "description": "Lorem Ipsum is simply dummy text of the printing industry.",
+    "url": "#",
+    "votes": 9,
+    "submitterAvatarUrl": "images/avatars/avatar2.png",
+    "productImageUrl": "images/products/green.png"
+  }
+]
+
+class ProductList extends React.Component {
+  render() {
+    const productsList = products.map((product) =>
+      <Product
+        key={'product-' + product.id}
+        id={product.id}
+        title={product.title}
+        description={product.description}
+        url={product.url}
+        votes={product.votes}
+        submitterAvatarUrl={product.submitterAvatarUrl}
+        productImageUrl={product.productImageUrl}
+      />
+    )
+    return (
+      <div className="ui unstackable items">
+        {productsList}
+      </div>
+    );
+  }
+}
+
+export default ProductList;
+
+
+class Product extends React.Component {
+  render() {
+    return (
+      <div className='item'>
+
+        <div className='image'>
+          <img src={this.props.productImageUrl} />
+        </div>
+
+        <div className='middle aligned content'>
+
+          <div className='header'>
+            <a>
+            <i className='large caret up icon' />
+            </a>
+            {this.props.votes}
+          </div>
+
+          <div className='description'>
+            <a href={this.props.url}>
+            {this.props.title}
+            </a>
+            <p>
+            {this.props.description}
+            </p>
+          </div>
+
+          <div className='extra'>
+            <span>Submitted by:</span>
+            <img
+            className='ui avatar image'
+            src={this.props.submitterAvatarUrl}
+            />
+          </div>
+
+        </div>
+
+      </div>
+    );
+  }
+}
